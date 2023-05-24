82d9c59254f9281a182ecc87983dc0e41a89d68d
samuel <samuelkelvinmwangi@gmail.com>
2023-05-24 08:48:15 +0300

    learning about children

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,16 @@ import Container from "./Container";
 function App() {
   return (
     <div className="App">
-      <Container header="Example header" direction="horizontal">
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
+      <Container header="Name" direction="horizontal">
+        <div>Jimmy!</div>
+        <div>Cindy!</div>
+        <div>sam!</div>
+        <div>Jane!</div>
+        <div>Janet!</div>
+        <div>Love!</div>
+        <div>Tom!</div>
       </Container>
-      <Container header="Example header" textPosition="center">
+      <Container header="Face" textPosition="center">
         <img
           alt="turtle"
           src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
